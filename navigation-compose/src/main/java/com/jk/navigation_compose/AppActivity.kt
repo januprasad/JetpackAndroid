0c8ce53906eec7c0588318970d002d156300dd8b
J . K <jnuprasad@gmail.com>
2022-07-27 22:47:21 +0530

    optimize imports

--- navigation-compose/src/main/java/com/jk/navigation_compose/AppActivity.kt
+++ navigation-compose/src/main/java/com/jk/navigation_compose/AppActivity.kt
@@ -2,26 +2,16 @@ package com.jk.navigation_compose
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Info
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import com.jk.navigation_compose.ui.theme.UdemyAppTheme
-import kotlinx.coroutines.flow.collectLatest
 
 class AppActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
